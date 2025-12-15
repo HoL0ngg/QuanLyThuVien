e0989d62f27c3d51a98cfbe62657202787d36a8f
QuocDai-210 <qdai.odin@gmail.com>
2025-12-15 23:39:57 +0700

    sua phieu muon

---
+++ QuanLyThuVien/GUI/DocGiaDialog.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace QuanLyThuVien.GUI
+{
+    partial class DocGiaDialog
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.groupBoxInfo = new System.Windows.Forms.GroupBox();
+            this.txtTrangThai = new System.Windows.Forms.TextBox();
+            this.lblTrangThai = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.lblDiaChi = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.lblSDT = new System.Windows.Forms.Label();
+            this.txtTenDG = new System.Windows.Forms.TextBox();
+            this.lblTenDG = new System.Windows.Forms.Label();
+            this.txtMaDG = new System.Windows.Forms.TextBox();
+            this.lblMaDG = new System.Windows.Forms.Label();
+            this.btnClose = new QuanLyThuVien.GUI.Components.CustomButton("Đóng", "red");
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.groupBoxInfo.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxInfo
+            // 
+            this.groupBoxInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxInfo.Controls.Add(this.txtTrangThai);
+            this.groupBoxInfo.Controls.Add(this.lblTrangThai);
+            this.groupBoxInfo.Controls.Add(this.txtDiaChi);
+            this.groupBoxInfo.Controls.Add(this.lblDiaChi);
+            this.groupBoxInfo.Controls.Add(this.txtSDT);
+            this.groupBoxInfo.Controls.Add(this.lblSDT);
+            this.groupBoxInfo.Controls.Add(this.txtTenDG);
+            this.groupBoxInfo.Controls.Add(this.lblTenDG);
+            this.groupBoxInfo.Controls.Add(this.txtMaDG);
+            this.groupBoxInfo.Controls.Add(this.lblMaDG);
+            this.groupBoxInfo.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxInfo.Name = "groupBoxInfo";
+            this.groupBoxInfo.Size = new System.Drawing.Size(417, 200);
+            this.groupBoxInfo.TabIndex = 0;
+            this.groupBoxInfo.TabStop = false;
+            this.groupBoxInfo.Text = "Thông tin độc giả";
+            // 
+            // txtTrangThai
+            // 
+            this.txtTrangThai.Location = new System.Drawing.Point(100, 147);
+            this.txtTrangThai.Name = "txtTrangThai";
+            this.txtTrangThai.ReadOnly = true;
+            this.txtTrangThai.ShortcutsEnabled = false;
+            this.txtTrangThai.Size = new System.Drawing.Size(300, 20);
+            this.txtTrangThai.TabIndex = 9;
+            this.txtTrangThai.TabStop = false;
+            // 
+            // lblTrangThai
+            // 
+            this.lblTrangThai.AutoSize = true;
+            this.lblTrangThai.Location = new System.Drawing.Point(16, 150);
+            this.lblTrangThai.Name = "lblTrangThai";
+            this.lblTrangThai.Size = new System.Drawing.Size(55, 13);
+            this.lblTrangThai.TabIndex = 8;
+            this.lblTrangThai.Text = "Trạng thái";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(100, 117);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.ReadOnly = true;
+            this.txtDiaChi.ShortcutsEnabled = false;
+            this.txtDiaChi.Size = new System.Drawing.Size(300, 20);
+            this.txtDiaChi.TabIndex = 7;
+            this.txtDiaChi.TabStop = false;
+            // 
+            // lblDiaChi
+            // 
+            this.lblDiaChi.AutoSize = true;
+            this.lblDiaChi.Location = new System.Drawing.Point(16, 120);
+            this.lblDiaChi.Name = "lblDiaChi";
+            this.lblDiaChi.Size = new System.Drawing.Size(40, 13);
+            this.lblDiaChi.TabIndex = 6;
+            this.lblDiaChi.Text = "Địa chỉ";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(100, 87);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.ReadOnly = true;
+            this.txtSDT.ShortcutsEnabled = false;
+            this.txtSDT.Size = new System.Drawing.Size(300, 20);
+            this.txtSDT.TabIndex = 5;
+            this.txtSDT.TabStop = false;
+            // 
+            // lblSDT
+            // 
+            this.lblSDT.AutoSize = true;
+            this.lblSDT.Location = new System.Drawing.Point(16, 90);
+            this.lblSDT.Name = "lblSDT";
+            this.lblSDT.Size = new System.Drawing.Size(29, 13);
+            this.lblSDT.TabIndex = 4;
+            this.lblSDT.Text = "SDT";
+            // 
+            // txtTenDG
+            // 
+            this.txtTenDG.Location = new System.Drawing.Point(100, 57);
+            this.txtTenDG.Name = "txtTenDG";
+            this.txtTenDG.ReadOnly = true;
+            this.txtTenDG.ShortcutsEnabled = false;
+            this.txtTenDG.Size = new System.Drawing.Size(300, 20);
+            this.txtTenDG.TabIndex = 3;
+            this.txtTenDG.TabStop = false;
+            // 
+            // lblTenDG
+            // 
+            this.lblTenDG.AutoSize = true;
+            this.lblTenDG.Location = new System.Drawing.Point(16, 60);
+            this.lblTenDG.Name = "lblTenDG";
+            this.lblTenDG.Size = new System.Drawing.Size(45, 13);
+            this.lblTenDG.TabIndex = 2;
+            this.lblTenDG.Text = "Tên DG";
+            // 
+            // txtMaDG
+            // 
+            this.txtMaDG.Location = new System.Drawing.Point(100, 27);
+            this.txtMaDG.Name = "txtMaDG";
+            this.txtMaDG.ReadOnly = true;
+            this.txtMaDG.ShortcutsEnabled = false;
+            this.txtMaDG.Size = new System.Drawing.Size(300, 20);
+            this.txtMaDG.TabIndex = 1;
+            this.txtMaDG.TabStop = false;
+            // 
+            // lblMaDG
+            // 
+            this.lblMaDG.AutoSize = true;
+            this.lblMaDG.Location = new System.Drawing.Point(16, 30);
+            this.lblMaDG.Name = "lblMaDG";
+            this.lblMaDG.Size = new System.Drawing.Size(41, 13);
+            this.lblMaDG.TabIndex = 0;
+            this.lblMaDG.Text = "Mã DG";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(327, 8);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 25);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Đóng";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelBottom.Controls.Add(this.btnClose);
+            this.panelBottom.Location = new System.Drawing.Point(12, 218);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(417, 40);
+            this.panelBottom.TabIndex = 1;
+            // 
+            // DocGiaDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 270);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.groupBoxInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DocGiaDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Thông tin độc giả";
+            this.groupBoxInfo.ResumeLayout(false);
+            this.groupBoxInfo.PerformLayout();
+            this.panelBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.GroupBox groupBoxInfo;
+        private System.Windows.Forms.Label lblMaDG;
+        private System.Windows.Forms.TextBox txtMaDG;
+        private System.Windows.Forms.Label lblTenDG;
+        private System.Windows.Forms.TextBox txtTenDG;
+        private System.Windows.Forms.Label lblSDT;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Label lblDiaChi;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label lblTrangThai;
+        private System.Windows.Forms.TextBox txtTrangThai;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ QuanLyThuVien/GUI/DocGiaDialog.cs
@@ -0,0 +1,28 @@
+﻿using QuanLyThuVien.DTO;
+using System;
+using System.Windows.Forms;
+
+namespace QuanLyThuVien.GUI
+{
+    public partial class DocGiaDialog : Form
+    {
+        public int SelectedMaDG { get; private set; }
+
+        public DocGiaDialog()
+        {
+            InitializeComponent();
+            btnClose.Click += (s, e) => { this.DialogResult = DialogResult.Cancel; this.Close(); };
+        }
+
+        public void SetDocGia(DocGiaDTO docGia)
+        {
+            if (docGia == null) return;
+            SelectedMaDG = docGia.MaDG;
+            txtMaDG.Text = docGia.MaDG.ToString();
+            txtTenDG.Text = docGia.TenDG;
+            txtSDT.Text = docGia.SDT;
+            txtDiaChi.Text = docGia.DiaChi;
+            txtTrangThai.Text = docGia.TrangThai == 1 ? "Hoạt động" : "Không hoạt động";
+        }
+    }
+}
